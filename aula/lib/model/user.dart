1ce9cdb2dcb0d63f0d02444465846e3f30314809
jhessik <jhessikkaelly.bezerraleal@gmail.com>
2024-08-13 10:34:57 -0300

    classe user

---
+++ aula/lib/model/user.dart
@@ -0,0 +1,12 @@
+class User {
+  //criando classe User
+  int? idade; // variavel que aceita valor null
+  String? nome;
+  double? peso;
+  double? altura;
+  bool? acimaDoPeso;
+  int idade1 = 20; // variavel que não aceita valor null
+  DateTime? hora;
+   
+// toda variavel varchar vira String
+}
